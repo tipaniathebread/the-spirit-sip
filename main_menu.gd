e74505cef3a1ec941a5835597018fd3851768ea1
tipania <tiffany.obina@cathcollege.school.nz>
2026-06-17 11:59:08 +1200

    coding main menus

---
+++ main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print("Start pressed")
+
+
+func _on_start_pressed() -> void:
+	print("Options pressed")
+
+
+func _on_options_pressed() -> void:
+	print("Exit pressed")
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
