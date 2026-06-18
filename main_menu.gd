12ed71031bdade86f6f4d0e6caf934f3b0b8986d
tipania <tiffany.obina@cathcollege.school.nz>
2026-06-18 13:28:46 +1200

    doing main menue options

--- main_menu.gd
+++ main_menu.gd
@@ -1,23 +1,30 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print("Start pressed")
+func _process(delta):
+	pass
 
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
 
 func _on_start_pressed() -> void:
-	print("Options pressed")
+	get_tree().change_scene_to_file("res://maincafe.tscn")
 
 
 func _on_options_pressed() -> void:
-	print("Exit pressed")
+	print("Options pressed")
+	main_buttons.visible = false
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
